8296e8c02c0695eb35628ec03ee847022d09fa6d
Jeremias Adriano <adrianojeremias1@gmail.com>
2023-03-10 19:53:18 +0200

    adicao da js

--- Main/js/index.js
+++ Main/js/index.js
@@ -1,3 +1,23 @@
-function bgColor(){
-    
+function colorH() {
+    let homi =  document.getElementById("homeLi");
+    homi.style.backgroundColor = "#ec6a30";
+    homi.style.transition = "none";  
+}
+function colorN() {
+    let news =  document.getElementById("homeLi");
+    news.style.backgroundColor = "#ec6a30";
+    news.style.transition = "ease-out .1s";
+
+}
+function colorP() {
+    let publi =  document.getElementById("homeLi");
+    publi.style.backgroundColor = "#ec6a30";
+    publi.style.transition = "ease-out .1s";
+
+}
+function colorM() {
+    let mag =  document.getElementById("homeLi");
+    mag.style.backgroundColor = "#ec6a30";
+    mag.style.transition = "ease-out .1s";
+
 }
