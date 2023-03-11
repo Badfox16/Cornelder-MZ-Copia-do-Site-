955a9afde338d8e56b18750126e8ddd6030699b2
Jeremias Adriano <adrianojeremias1@gmail.com>
2023-03-11 11:36:57 +0200

    Onload adicao(isso deve ser alterado de header para header, page em page)

--- Main/js/index.js
+++ Main/js/index.js
@@ -2,22 +2,27 @@ function colorH() {
     let homi =  document.getElementById("homeLi");
     homi.style.backgroundColor = "#ec6a30";
     homi.style.transition = "none";  
+    homi.style.paddingTop="30px";
+    homi.style.color="white";
 }
 function colorN() {
     let news =  document.getElementById("homeLi");
     news.style.backgroundColor = "#ec6a30";
     news.style.transition = "ease-out .1s";
+    news.style.paddingTop="30px";
 
 }
 function colorP() {
     let publi =  document.getElementById("homeLi");
     publi.style.backgroundColor = "#ec6a30";
     publi.style.transition = "ease-out .1s";
+    publi.style.paddingTop="30px";
 
 }
 function colorM() {
     let mag =  document.getElementById("homeLi");
     mag.style.backgroundColor = "#ec6a30";
     mag.style.transition = "ease-out .1s";
+    mag.style.paddingTop="30px";
 
 }
