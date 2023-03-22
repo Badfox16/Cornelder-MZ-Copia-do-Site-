e947eaea6f8c000aa1509bf79b5430b462218a94
Jeremias Adriano <adrianojeremias1@gmail.com>
2023-03-22 10:33:19 +0200

    Texto fixe

--- Main/js/index.js
+++ Main/js/index.js
@@ -5,49 +5,49 @@ showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides((slideIndex += n));
+   showSlides((slideIndex += n));
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides((slideIndex = n));
+   showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+   let i;
+   let slides = document.getElementsByClassName("mySlides");
+   let dots = document.getElementsByClassName("dot");
+   if (n > slides.length) {
+      slideIndex = 1;
+   }
+   if (n < 1) {
+      slideIndex = slides.length;
+   }
+   for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+   }
+   for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+   }
+   slides[slideIndex - 1].style.display = "block";
+   dots[slideIndex - 1].className += " active";
 }
 
 function showSlidesAuto() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-  slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlidesAuto, 6000); 
+   let i;
+   let slides = document.getElementsByClassName("mySlides");
+   for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+   }
+   slideIndex++;
+   if (slideIndex > slides.length) {
+      slideIndex = 1;
+   }
+   slides[slideIndex - 1].style.display = "block";
+   setTimeout(showSlidesAuto, 6000);
 }
 
 // Acao do botao close no slideShow
-function closeYt(){
-  window.document.querySelector(".seeYoutube").style.display="none";
+function closeYt() {
+   window.document.querySelector(".seeYoutube").style.display = "none";
 }
